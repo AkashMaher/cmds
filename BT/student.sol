61d9804723dca38569f4b0f0e782622cd51966f1
Akash <aakashrajp5@gmail.com>
2022-11-20 20:35:24 +0530

    uploaded

---
+++ BT/student.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.8.0;
+
+
+contract student{
+
+    struct stud{
+        uint256 id;
+        string name;
+        string class;
+        string department;
+    }
+    stud[] public Students;
+    uint256 _ids;
+
+
+    function addStudent(string memory name, string memory class, string memory department) public {
+        
+        uint256 id = _ids;
+        stud memory data = stud(id,name, class, department);
+        Students.push(data);
+        _ids +=1;
+    }
+
+    function getStudent(uint256 _id) public view returns(uint256 id,string memory name, string memory class, string memory department){
+        require(_id<_ids,"students Id is not present");
+        
+        for(uint i; i<Students.length;i++){
+
+            if(Students[i].id == _id){
+                id = Students[i].id;
+                name = Students[i].name;
+                class= Students[i].class;
+                department = Students[i].department;
+                return(id,name,class,department);
+            }
+        }
+    }
+
+    function totalStudents() public view returns(uint256){
+        return(_ids);
+    }
+
+    
+
+}
