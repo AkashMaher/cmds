ecd9fe2f5d2c142f8fd12054681ef306cdcaf3d1
Akash <aakashrajp5@gmail.com>
2022-11-20 23:37:45 +0530

    update: bank

--- BT/bank.sol
+++ BT/bank.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0;
 
 contract bank_account{
@@ -7,20 +7,24 @@ contract bank_account{
     mapping(address => bool) public is_user;
 
     function create_account() public {
-        require(!is_user[msg.sender], "Account already exist");
+        require(is_user[msg.sender] == false, "Account already exist");
         is_user[msg.sender] = true;
     }
 
-    function deposit(uint256 amount) external payable {
+    function deposit() public payable {
         require(is_user[msg.sender],"User Account Not Found");
-        require(msg.value>=amount,"You don't have sent enough amount to deposit");
-        user_balance[msg.sender] += amount;  
+        user_balance[msg.sender] += msg.value;  
     }
 
-    function withdraw(uint256 amount) external payable {
+    function withdraw(uint256 amount) public {
         require(is_user[msg.sender],"User Account Not Found");
         require(user_balance[msg.sender]>=amount,"You don't have enough balance to withdraw");
         require(payable(msg.sender).send(amount));
         user_balance[msg.sender] -= amount; 
+    }
+
+    function show_balance(address user) public view returns(uint){
+        require(is_user[user],"User Account Not Found");
+        return(user_balance[user]);
     }
 }
