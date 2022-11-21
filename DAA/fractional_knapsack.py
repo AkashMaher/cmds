afe17ffc3c7d8f30b746c11c35a2c280210ae92f
Akash <aakashrajp5@gmail.com>
2022-11-21 09:51:45 +0530

    update

--- DAA/fractional_knapsack.py
+++ DAA/fractional_knapsack.py
@@ -2,18 +2,21 @@ def fractional():
     weight = [10,20,30]
     value = [60,100,120]
     WT = 50
-    res = 0
+    res = 0.0
     
-    for pair in sorted(zip(weight,value), key= lambda x: x[1]/x[0], reverse=True):
-        if WT <= 0:
+    for pair in sorted(zip(weight, value), key=lambda x:x[1]/x[0], reverse=True):
+        if(WT<=0):
             break
-        if pair[0]>WT:
-            res+=int(WT * (pair[1]/pair[0]))
+        if WT<pair[0]:
+            res += (WT * pair[1]/pair[0])
             WT = 0
-        elif pair[0]<=WT:
-            res+=pair[1]
+        else :
             WT -= pair[0]
+            res += pair[1]
     print(res)
-
+        
 fractional() 
+
+
+
    
